4d68e6e6eef7ce067139a1e188fd1ea435d88de6
jxrvll <jxrvll@gmail.com>
2020-08-20 18:07:49 -0400

    Add start script

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
